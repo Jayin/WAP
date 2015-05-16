a25cf4f11aff0e0470cff49188ed399229414904
Jayin Ton <tonjayin@gmail.com>
2015-05-16 22:18:08 +0800

    automatically load the module in folder api/v1/

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,10 +1,15 @@
 var express = require('express')
 var router = express.Router()
+var fs = require('fs')
 
-var e = require('./event')
+//加载当前目录下所有 非index模块
+fs.readdirSync(__dirname).forEach(function(filename){
+    if (filename !== 'index.js'){
+        var module_name = filename.substring(0, filename.lastIndexOf('.'))
+        router.use('/' + module_name, require(__dirname + '/' + module_name))
+    }
+})
 
-
-router.use('/event',e)
 
 module.exports = router;
 
